f38b176c7b845305253ea6f8f967622b0c28d916
samanvayzed <50007783+samanvayzed@users.noreply.github.com>
2019-07-30 12:58:44 +0530

    Update live_minutely_cron.sh

--- live_minutely_cron.sh
+++ live_minutely_cron.sh
@@ -2,14 +2,14 @@
 
 source ~/env/bin/activate
 #which python
-x=`ps -ef | grep -v "grep" | grep '/home/manishkumar_zed/mirai/live/jts-flask-api/main.py' | awk '{ print $2 }'`
+x=`ps -ef | grep -v "grep" | grep '/root/mirai/live/jts-flask-api/main.py' | awk '{ print $2 }'`
 echo ${x}
 
 
 if [ -z "$x" ]
 then
      echo "\$x is empty"
-     cd /home/manishkumar_zed/mirai/live/jts-flask-api
-     /home/manishkumar_zed/env/bin/python /home/manishkumar_zed/mirai/live/jts-flask-api/main.py
+     cd /root/mirai/live/jts-flask-api
+     /root/env/bin/python /root/mirai/live/jts-flask-api/main.py
 fi
 
