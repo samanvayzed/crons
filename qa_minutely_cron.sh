6156e1baa41f16769a49f2a35abec9479f660a1d
samanvayzed <50007783+samanvayzed@users.noreply.github.com>
2019-07-30 12:59:56 +0530

    Update qa_minutely_cron.sh

--- qa_minutely_cron.sh
+++ qa_minutely_cron.sh
@@ -2,15 +2,15 @@
 
 source ~/env/bin/activate
 #which python
-x=`ps -ef | grep -v "grep" | grep '/home/manishkumar_zed/mirai/qa/jts-flask-api/main.py' | awk '{ print $2 }'`
+x=`ps -ef | grep -v "grep" | grep '/root/mirai/qa/jts-flask-api/main.py' | awk '{ print $2 }'`
 echo ${x}
 
 
 if [ -z "$x" ]
 then
      echo "\$x is empty"
-     cd /home/manishkumar_zed/mirai/qa/jts-flask-api
-     /home/manishkumar_zed/env/bin/python /home/manishkumar_zed/mirai/qa/jts-flask-api/main.py
+     cd /root/mirai/qa/jts-flask-api
+     /root/env/bin/python /root/mirai/qa/jts-flask-api/main.py
 fi
 
 
